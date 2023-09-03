70c3de2cf892c6f63216f0006828f35647720888
lemon-lime-honey <bs0308@g.skku.edu>
2023-09-03 23:58:19 +0900

    Add serializers
    - PostSerializer, PostListSerializer 작성

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,2 +1,14 @@
 from rest_framework import serializers
 from .models import *
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class PostListSerializer(serializers.ModelField):
+    class Meta:
+        model = Post
+        fields = '__all__'
