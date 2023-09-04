35b82a23386751d1baa0ed863f0e6ee3d0f9aff1
lemon-lime-honey <bs0308@g.skku.edu>
2023-09-04 11:57:16 +0900

    Add views
    - PostViewSet 추가
    - 메서드 get_serializer_class, retrieve 재정의

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    renderer_classes = [JSONRenderer]
+
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return PostListSerializer
+        else:
+            return PostSerializer
+
+    def retrieve(self, request, pk):
+        post = self.queryset.get(pk=pk)
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(instance=post)
+        data = serializer.data
+        data['breadcrumbs'] = ['Post', serializer.data['category'], serializer.data['title']]
+        return Response(data=data)
